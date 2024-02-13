54e7a875442d19e3f8810e9332e495614d023f2d
Aigooooo <alcoveredgarjr@gmail.com>
2024-02-13 15:00:03 +0800

    First commit

---
+++ Hanajin/script.js
@@ -0,0 +1,194 @@
+var btn = document.getElementById("continue-btn");
+var count = 0;
+btn.addEventListener("click", function () {
+  var button = btn.id;
+  console.log("Click " + count);
+  count++;
+  switch (count) {
+    case 1:
+      var msg = document.getElementById("message");
+      msg.innerHTML = "Did you have <br/>a good day?<br/>૮₍ ˶ᵔ ᵕ ᵔ˶ ₎ა";
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://media4.giphy.com/media/RIKHEPa1OHKUhtLT5m/200.webp?cid=790b7611zwg3edbqjsr34aea3wph86i4jp3q0qbp87tkbnb3&ep=v1_gifs_search&rid=200.webp&ct=g"
+      );
+      var imgContainer = document.getElementById("imgContainer");
+      imgContainer.appendChild(imageElement);
+      break;
+    case 2:
+      var msg = document.getElementById("message");
+      msg.innerHTML = "Because I did <br/> ( ᴗ͈ˬᴗ͈)ഒ";
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://media1.giphy.com/media/1wpd0ErqTotMKZEWn7/200.webp?cid=790b7611an95k167ktzoicrsvv8792nowabfggmb6klfteha&ep=v1_gifs_search&rid=200.webp&ct=g"
+      );
+      var imgContainer = document.getElementById("imgContainer");
+      imgContainer.appendChild(imageElement);
+      break;
+    case 3:
+      var msg = document.getElementById("message");
+      msg.innerHTML =
+        "A lot of things<br/> have been weighing on <br/>your mind lately";
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://media0.giphy.com/media/GwskZm1jXg8cDvuZJ6/giphy.webp?cid=790b7611ddhvad48lbm9dapw83yiyy1jx8sy3to7acx4ftt3&ep=v1_gifs_search&rid=giphy.webp&ct=g"
+      );
+      var imgContainer = document.getElementById("imgContainer");
+      imgContainer.appendChild(imageElement);
+      break;
+    case 4:
+      var msg = document.getElementById("message");
+      msg.innerHTML = "you've been so strong<br>(ง'̀-'́)ง";
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://media0.giphy.com/media/hV0pccEE0jLfelZPCC/giphy.webp?cid=790b7611nfiuuws4novvnc7uzd0u87s7v70x0h1g3n5gpez4&ep=v1_gifs_search&rid=giphy.webp&ct=g"
+      );
+      var imgContainer = document.getElementById("imgContainer");
+      imgContainer.appendChild(imageElement);
+      break;
+    case 5:
+      var msg = document.getElementById("message");
+      msg.innerHTML = "and I might not know it all <br/>( •̯́ ^ •̯̀)";
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://media.tenor.com/_nPtjSWMT6kAAAAe/cat-meme-cat-sigh.png"
+      );
+      var imgContainer = document.getElementById("imgContainer");
+      imgContainer.appendChild(imageElement);
+      break;
+    case 6:
+      var msg = document.getElementById("message");
+      msg.innerHTML = "but just know that...";
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://gifdb.com/images/high/cute-cat-playful-chilling-guqncj98py7aest8.gif"
+      );
+      var imgContainer = document.getElementById("imgContainer");
+      imgContainer.appendChild(imageElement);
+      break;
+    case 7:
+      var msg = document.getElementById("message");
+      msg.innerHTML = "I'll always be your <br/>companion<br/>(◡ ‿ ◡ .)";
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://imgflip.com/s/meme/Smiling-Cat.jpg"
+      );
+      var imgContainer = document.getElementById("imgContainer");
+      imgContainer.appendChild(imageElement);
+      break;
+    case 8:
+      var msg = document.getElementById("message");
+      msg.innerHTML = `your "pahinga" ᶻ 𝗓 𐰁`;
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://media3.giphy.com/media/1FkCqpyObTuo0/giphy.webp?cid=ecf05e47oys9hnx50w8ao1h39di0xlgfv3df2dja69bcz0t9&ep=v1_gifs_search&rid=giphy.webp&ct=g"
+      );
+      var imgContainer = document.getElementById("imgContainer");
+      imgContainer.appendChild(imageElement);
+      break;
+    case 9:
+      var msg = document.getElementById("message");
+      msg.innerHTML = "Anyways...";
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMXo1bXc3YWE4cXo1ZnZuMXpwc2swZWR2NTFrYnhkZWJuemNvaXBnaCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/5fsKJ8LtychlzdDQj0/giphy.webp"
+      );
+      var imgContainer = document.getElementById("imgContainer");
+      imgContainer.appendChild(imageElement);
+      break;
+    case 10:
+      var msg = document.getElementById("message");
+      msg.innerHTML = "tomorrow is February 14";
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://media0.giphy.com/media/7SfAXqgRgh0li/200.webp?cid=ecf05e47szq0lp02msp5zl2df62ribe0elour3k9owp1y01n&ep=v1_gifs_search&rid=200.webp&ct=g"
+      );
+      var imgContainer = document.getElementById("imgContainer");
+      imgContainer.appendChild(imageElement);
+      break;
+    case 11:
+      var msg = document.getElementById("message");
+      msg.innerHTML = "This might be a little late";
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://media1.giphy.com/media/yFQ0ywscgobJK/giphy.webp?cid=790b7611ptaiaza3ere5kzg089iska6rjk6zktrwkeur9nk2&ep=v1_gifs_search&rid=giphy.webp&ct=g"
+      );
+      var imgContainer = document.getElementById("imgContainer");
+      imgContainer.appendChild(imageElement);
+      break;
+    case 12:
+      var msg = document.getElementById("message");
+      msg.innerHTML = "I mean... very late";
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcgoOp_0FpgYNgXDMNAguaSllm2i96CLQ1ew&usqp=CAU"
+      );
+      var imgContainer = document.getElementById("imgContainer");
+      imgContainer.appendChild(imageElement);
+      break;
+    case 13:
+      var msg = document.getElementById("message");
+      msg.innerHTML = "But...";
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://media.tenor.com/cKWYvIOB324AAAAM/cat-scared.gif"
+      );
+      var imgContainer = document.getElementById("imgContainer");
+      imgContainer.appendChild(imageElement);
+      break;
+    case 14:
+      var msg = document.getElementById("message");
+      msg.innerHTML = "Can you be my valentine?";
+
+      var imageElement = document.getElementById("imgId");
+      imageElement.setAttribute(
+        "src",
+        "https://i.pinimg.com/originals/81/19/4d/81194d5bd1813c2ad03796cb5f3bfb2e.gif"
+      );
+
+      var mainContainer = document.getElementsByClassName("content")[0];
+      var continueButton = document.getElementById("continue-btn");
+
+      // Hide the continue button
+      continueButton.style.display = "none";
+
+      // Create and append the optionContainer
+      var optionContainer = document.createElement("div");
+      optionContainer.setAttribute("id", "optionContainer");
+      mainContainer.appendChild(optionContainer);
+
+      var option1 = document.createElement("button");
+      var option2 = document.createElement("button");
+      option1.setAttribute("class", "btn btn-outline-light");
+      option2.setAttribute("class", "btn btn-outline-light");
+      option1.textContent = "Yes";
+      option2.textContent = "No";
+
+      optionContainer.appendChild(option1);
+      optionContainer.appendChild(option2);
+
+      // Disable the second button
+      option2.disabled = true;
+
+      // Add a click event listener to the first button
+      option1.addEventListener("click", function () {
+        // Change the URL to the desired page
+        window.location.href = "hooray.html";
+      });
+
+      break;
+  }
+});
